7aa65083e71d2dc8f115e941fa8c3fff04c37683
Mi1-programming <138886795+Mi1-programming@users.noreply.github.com>
2024-01-09 16:09:30 +0200

    Fixes

--- Jenga/Assets/Scripts/GameManager.cs
+++ Jenga/Assets/Scripts/GameManager.cs
@@ -6,7 +6,14 @@ public class GameManager : MonoBehaviour
     public static bool hasLost;
     [SerializeField] private string keepTag;
     [SerializeField] private UnityEvent onLose;
+    [SerializeField] private UnityEvent onStart;
 
+    private void Start()
+    {
+        hasLost = false;
+        onStart.Invoke();
+        Time.timeScale = 1.0f;
+    }
     public void Lose()
     {
         hasLost = true;
